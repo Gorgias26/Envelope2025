e26be17d708d7cc7140889c1267b4e16816a7d1c
Gorgias26 <37674056+Gorgias26@users.noreply.github.com>
2025-03-15 00:40:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,14 +49,14 @@ pip install -r requirements.txt
 git update-index --assume-unchanged secret.py
 
 # Configuration des tâches cron
-(crontab -l 2>/dev/null; echo "*/5 * * * * cd \"$(pwd)\" && /bin/bash ./BtcX1.sh >> \"$(pwd)/log/BtcX1.log\" 2>&1") | crontab -
-(crontab -l 2>/dev/null; echo "0-59/5 * * * * cd \"$(pwd)\" && sleep 30 && /bin/bash ./BtcX2.sh >> \"$(pwd)/log/BtcX2.log\" 2>&1") | crontab -
-(crontab -l 2>/dev/null; echo "1-59/5 * * * * cd \"$(pwd)\" && /bin/bash ./BtcX5.sh >> \"$(pwd)/log/BtcX5.log\" 2>&1") | crontab -
-(crontab -l 2>/dev/null; echo "1-59/5 * * * * cd \"$(pwd)\" && sleep 30 && /bin/bash ./BtcX10.sh >> \"$(pwd)/log/BtcX10.log\" 2>&1") | crontab -
-(crontab -l 2>/dev/null; echo "2-59/5 * * * * cd \"$(pwd)\" && /bin/bash ./BtcX20.sh >> \"$(pwd)/log/BtcX20.log\" 2>&1") | crontab -
-(crontab -l 2>/dev/null; echo "2-59/5 * * * * cd \"$(pwd)\" && sleep 30 && /bin/bash ./BtcX30.sh >> \"$(pwd)/log/BtcX30.log\" 2>&1") | crontab -
-(crontab -l 2>/dev/null; echo "3-59/5 * * * * cd \"$(pwd)\" && /bin/bash ./AltX3High.sh >> \"$(pwd)/log/AltX3High.log\" 2>&1") | crontab -
-(crontab -l 2>/dev/null; echo "3-59/5 * * * * cd \"$(pwd)\" && sleep 30 && /bin/bash ./AltX3Traling.sh >> \"$(pwd)/log/AltX3Traling.log\" 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * bash Envelope2025/BtcX1.sh >> log/BtcX1.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "0-59/5 * * * * sleep 30 && bash Envelope2025/BtcX2.sh >> log/BtcX2.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "1-59/5 * * * * bash Envelope2025/BtcX5.sh >> log/BtcX5.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "1-59/5 * * * * sleep 30 && bash Envelope2025/BtcX10.sh >> log/BtcX10.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "2-59/5 * * * * bash Envelope2025/BtcX20.sh >> log/BtcX20.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "2-59/5 * * * * sleep 30 && bash Envelope2025/BtcX30.sh >> log/BtcX30.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "3-59/5 * * * * bash Envelope2025/AltX3High.sh >> log/AltX3High.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "3-59/5 * * * * sleep 30 && bash Envelope2025/AltX3Traling.sh >> log/AltX3Traling.log 2>&1") | crontab -
 
 cd ..
 echo "Installation terminée !"
