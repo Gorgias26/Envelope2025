9981a0e843018a60eb1de386ad2e4b5781f5d019
Gorgias26 <37674056+Gorgias26@users.noreply.github.com>
2025-03-15 00:01:56 +0100

    Add files via upload

--- BtcX1.sh
+++ BtcX1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source "Live-Tools-V2-main 2025/.venv/bin/activate"
+source "Envelope2025/.venv/bin/activate"
 python3 BtcX1.py
--- BtcX10.sh
+++ BtcX10.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source "Live-Tools-V2-main 2025/.venv/bin/activate"
+source "Envelope2025/.venv/bin/activate"
 python3 BtcX10.py
